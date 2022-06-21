2d4650108703fb4fd26752f9d1623fb117887b92
kode <kode.creer@gmail.com>
2022-06-21 11:40:07 -0600

    added new line

--- main.s
+++ main.s
@@ -1,5 +1,13 @@
 global _start
 EXTERN getn, generate_random_num, printn, print, 
+
+section .data
+    nl:
+        db `\n`,0
+    lnl:
+        equ $ - nl
+    x: 
+      dd 0
 section .text
 ;%include "std.s"
 %include "io.h"
@@ -9,7 +17,10 @@ _start:
     mov eax, ecx
     mov ebx, 2
     call printn
-
+    mov dword [x], ecx
+    mov eax, nl
+    mov ebx, lnl
+    call print
 
     mov eax, EXIT
     mov ebx, 0;everything worked
